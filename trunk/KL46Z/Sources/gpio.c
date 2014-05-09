a41a7f408460c9d0cee8ca036bda0115cd648731
severin.davis@gmail.com <severin.davis@gmail.com@d5b178a0-251e-a8bb-41f0-a278b1469bd0>
2014-05-09 23:45:47 +0000

    gpio should be working now

--- trunk/KL46Z/Sources/gpio.c
+++ trunk/KL46Z/Sources/gpio.c
@@ -1,18 +1,18 @@
 #include <gpio.h>
 
-#define PORTX_PCR_BASE 			PORTA_PCR0 
+#define PORTX_PCR_BASE 			0x40049000
 
-#define FGPIOX_PDOR_BASE    	FGPIOA_PDOR                    
-#define FGPIOX_PSOR_BASE    	FGPIOA_PSOR            
-#define FGPIOX_PCOR_BASE    	FGPIOA_PCOR         
-#define FGPIOX_PTOR_BASE    	FGPIOA_PTOR                  
-#define FGPIOX_PDIR_BASE    	FGPIOA_PDIR                        
-#define FGPIOX_PDDR_BASE    	FGPIOA_PDDR
+#define FGPIOX_PDOR_BASE    	0xF80FF000                    
+#define FGPIOX_PSOR_BASE    	0xF80FF004            
+#define FGPIOX_PCOR_BASE    	0xF80FF008        
+#define FGPIOX_PTOR_BASE    	0xF80FF00C                  
+#define FGPIOX_PDIR_BASE    	0xF80FF010                       
+#define FGPIOX_PDDR_BASE    	0xF80FF014
 
 
 void gpio_pin_set_dir(port_t p_port, pin_t p_pin, dir_t p_dir)
 {
-	int * custom_PDDR = (int *)(FGPIOX_PDDR_BASE + (p_port * 0x40));
+	unsigned int * custom_PDDR = (unsigned int *)(FGPIOX_PDDR_BASE + (p_port * 0x40));
 	
 	*custom_PDDR &= 1 << p_pin;
 	
@@ -21,14 +21,12 @@ void gpio_pin_set_dir(port_t p_port, pin_t p_pin, dir_t p_dir)
 
 void gpio_pin_set_alt(port_t p_port, pin_t p_pin, alt_t p_alt)
 {
-	int test = PORTX_PCR_BASE;
-	unsigned int * custom_PCR = (PORTX_PCR_BASE);
-	custom_PCR += (p_port * 0x1000);
-	custom_PCR += (p_pin * 0x4);
-	
-	*custom_PCR &= ~0x700;
-	
-	*custom_PCR |= p_alt << 8;
+    unsigned int * custom_PCR = (unsigned int *)(PORTX_PCR_BASE + p_port * 0x1000 + p_pin * 0x4);
+
+    
+    *custom_PCR &= ~0x700;
+    
+    *custom_PCR |= p_alt << 8;
 }
 
 void gpio_port_init(port_t p_port, pin_t p_pin, alt_t p_alt, dir_t p_dir)
@@ -37,13 +35,13 @@ void gpio_port_init(port_t p_port, pin_t p_pin, alt_t p_alt, dir_t p_dir)
 	
 	gpio_pin_set_alt(p_port, p_pin, p_alt);
 	
-	gpio_pin_set_dir(p_port, p_pin, p_alt);
+	gpio_pin_set_dir(p_port, p_pin, p_dir);
 	
 }
 
 int gpio_get_pin_state(port_t p_port, pin_t p_pin)
 {
-	int * custom_PDOR = (int *)(FGPIOX_PDOR_BASE + (p_port * 0x40));
+	unsigned int * custom_PDOR = (unsigned int *)(FGPIOX_PDOR_BASE + (p_port * 0x40));
 			
 	return (*custom_PDOR >> p_pin) & 1;
 }
@@ -53,23 +51,23 @@ void gpio_set_pin_state(port_t p_port, pin_t p_pin, int p_state)
 {
 	if(p_state)
 	{
-		int * custom_PSOR = (int *)(FGPIOX_PSOR_BASE + (p_port * 0x40));
+		unsigned int * custom_PSOR = (unsigned int *)(FGPIOX_PSOR_BASE + (p_port * 0x40));
 		
-		*custom_PSOR &= 1 << p_pin;	
+		*custom_PSOR = 1 << p_pin;	
 	}
 	
 	else
 	{
-		int * custom_PCOR = (int *)(FGPIOX_PCOR_BASE + (p_port * 0x40));
+		unsigned int * custom_PCOR = (unsigned int *)(FGPIOX_PCOR_BASE + (p_port * 0x40));
 		
-		*custom_PCOR &= 1 << p_pin;	
+		*custom_PCOR = 1 << p_pin;	
 		
 	}
 }
 
 void gpio_toggle_pin_state(port_t p_port, pin_t p_pin)
 {
-	int * custom_PTOR = (int *)(FGPIOX_PTOR_BASE + (p_port * 0x40));
+	unsigned int * custom_PTOR = (unsigned int *)(FGPIOX_PTOR_BASE + (p_port * 0x40));
 	
-	*custom_PTOR &= 1 << p_pin;	
+	*custom_PTOR = 1 << p_pin;	
 }
