3f4761496463ef0adc6cf29b81ead88be425a3e1
severin.davis@gmail.com <severin.davis@gmail.com@d5b178a0-251e-a8bb-41f0-a278b1469bd0>
2014-06-08 23:32:12 +0000

    dac working

---
+++ trunk/Sources/uc_dac.c
@@ -0,0 +1,17 @@
+
+
+#include "uc_dac.h"
+
+void uc_dac_init()
+{
+	gpio_port_init(port_E, pin_30, alt_0, output);
+	SIM_SCGC6  |= 1 << 31;
+	DAC0_C0 |= 1 << 6;
+	DAC0_C0 |= 1 << 7;
+}
+
+void uc_dac_set_output(int p_numerator)
+{
+	DAC0_DAT0L = p_numerator;
+	DAC0_DAT0H = (p_numerator >> 8);
+}
