5302350ce242119a8c8698265cd64d6c60c6a904
severin.davis@gmail.com <severin.davis@gmail.com@d5b178a0-251e-a8bb-41f0-a278b1469bd0>
2014-06-09 03:10:54 +0000

    pit should be working. nvic needs attention and pit needs to be generalized.

--- trunk/Sources/main.c
+++ trunk/Sources/main.c
@@ -14,6 +14,7 @@
 #include "gpio.h"
 #include "uc_lptmr.h"
 #include "uc_dac.h"
+#include "uc_pit.h"
 
 void clock_init()
 {
@@ -30,13 +31,12 @@ int main(void)
 	uc_lptmr_init();
 	
 	int a = 100;
-	
+	pit_init(0, priority_1, 0);
 	while(1)
 	{
 		uc_led_toggle(led_red);
-		uc_dac_set_output(3600);
-		a += 500;
-		uc_lptmr_delay(3000);
+
+		uc_lptmr_delay(1000);
 	}
 	return 0;
 }
--- trunk/Sources/uc_pit.c
+++ trunk/Sources/uc_pit.c
@@ -6,6 +6,7 @@
  */
 
 #include "uc_pit.h"
+#include "uc_led.h"
 
 void pit_enable(pit_t p_timer)
 {
@@ -20,7 +21,20 @@ void pit_disable(pit_t p_timer)
 void pit_init(pit_t p_timer, priority_t p_priority, int p_period)
 {
 	SIM_SCGC6 |= 1<<23;
+	/*PIT_MCR &= ~(1 << 1);
 	int_init(INT_PIT, p_priority);
+	PIT_TCTRL0 |=0b11;*/
+	
+	// turn on PIT
+	PIT_MCR = 0x00;
+	int_all_unmask();
+	NVIC_ICPR |= 1 << ((INT_PIT - 16) % 32);
+	NVIC_ISER |= 1 << ((INT_PIT - 16) % 32);
+	int_init(INT_PIT, p_priority);
+	// Timer 1
+	PIT_LDVAL0 = 24000000; // setup timer 1 for 256000 cycles
+	PIT_TCTRL0 |=0b11;
+
 }
 
 void pit_set_callback(pit_t p_timer, callback_t p_callback)
@@ -32,5 +46,7 @@ void pit_set_callback(pit_t p_timer, callback_t p_callback)
 //ISR
 void PIT_IRQHandler()
 {
+	PIT_TFLG0 = 1;
 	
+	uc_led_toggle(led_green);
 }
