f489dc273ee0d0cb4152da0a97318046459df19d
severin.davis@gmail.com <severin.davis@gmail.com@d5b178a0-251e-a8bb-41f0-a278b1469bd0>
2014-11-01 09:27:45 +0000

    preliminary CAR_MOTOR stuff written. Next step is to confirm bit order from sh_regs to mdrivers/ write setter methods to update mdrivers settings.

--- trunk/Sources/uc_dac.c
+++ trunk/Sources/uc_dac.c
@@ -5,10 +5,15 @@ void uc_dac_init()
 {
 	gpio_port_init(port_E, pin_30, alt_0, output);
 	SIM_SCGC6  |= 1 << 31;
+	uc_dac_set_output(0);
+	
 	DAC0_C0 |= 1 << 6;
 	DAC0_C0 |= 1 << 7;
 }
 
+//p_numerator is out of 4095. VOUT = VIN * (1+p_numerator)/4096
+//MAX VOUT = VIN
+//MIN VOUT = (1/4096) * VIN
 void uc_dac_set_output(int p_numerator)
 {
 	DAC0_DAT0L = p_numerator;
