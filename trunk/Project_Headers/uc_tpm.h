e03c692042e8942e230377fea5c5929aff1858fd
severin.davis@gmail.com <severin.davis@gmail.com@d5b178a0-251e-a8bb-41f0-a278b1469bd0>
2014-11-22 22:18:00 +0000

    tpm module working, somewhat tested.

--- trunk/Project_Headers/uc_tpm.h
+++ trunk/Project_Headers/uc_tpm.h
@@ -9,8 +9,20 @@
 #define UC_TPM_H_
 
 #include "derivative.h"
+#include "gpio.h"
+#include "int.h"
+
+typedef enum{
+	tpm_chan_0,
+	tpm_chan_1,
+	tpm_chan_2,
+	ttpm_chan_3,
+	tpm_chan_4,
+	tpm_chan_5
+} tpm_chan_t;
 
 void uc_tpm_init();
+void uc_tpm_set_callback(tpm_chan_t, callback_t);
 
 
 #endif /* UC_TPM_H_ */
--- trunk/Sources/uc_tpm.c
+++ trunk/Sources/uc_tpm.c
@@ -6,44 +6,82 @@
  */
 
 #include "uc_tpm.h"
+#include "uc_led.h"
 
-#define TPM_SC_BASE 	0x40038000
-#define TPM_CNT_BASE	0x40038004
-#define TPM_MOD_BASE	0x40038008
-#define TPM_CNSC_BASE	0x4003800C
-#define TPM_CNV_BASE	0x40038010
-#define TPM_STATUS_BASE	0x40038050
-#define TPM_CONF_BASE	0x40038084
+
+#define TPM_MOD_VAL 0xFFFE
+#define TPM_MOD_VAL_OFF 0xFFFF
+
+static callback_t tpm_callback[6] ={0,0,0,0,0,0};
 
 void uc_tpm_init()
-
 {
+		//motors are driven off D2-D4 outputs with TPM function (alt4)
+		gpio_port_init(port_D, pin_2, alt_4, 1);
+		gpio_port_init(port_D, pin_3, alt_4, 1);
+		gpio_port_init(port_D, pin_4, alt_4, 1);
+		gpio_port_init(port_D, pin_5, alt_4, 1);
 
-       //PA1 as TPM
-       SIM_BASE_PTR->SCGC5 |= SIM_SCGC5_PORTA_MASK;
-       PORTA_BASE_PTR->PCR[1] = PORT_PCR_MUX(3);
-       PTA_BASE_PTR->PDDR = 1<< 1;
+		//internal clock selected for FLL and MCGIRCLK active
+		MCG_C1 |= MCG_C1_IRCLKEN_MASK | MCG_C1_IREFS_MASK;
+		//choose fast internal reference clock
+		MCG_C2 |= MCG_C2_IRCS_MASK;
 
-       MCG_C1 |= 0b11 << 1;
-       MCG_C2 |= 1;
+		//enable clock to tpm 
+		SIM_SCGC6 |= SIM_SCGC6_TPM0_MASK;
+		//choose MCGIRCLK as TPM clk source
+		SIM_SOPT2 |= SIM_SOPT2_TPMSRC(3);
 
-       SIM_SCGC6 |= SIM_SCGC6_TPM2_MASK;
-       SIM_SOPT2 |= SIM_SOPT2_TPMSRC(3);
-
-       TPM2_BASE_PTR->SC = TPM_SC_CMOD(1) |TPM_SC_PS(6);
-       TPM2_BASE_PTR->MOD=0xFFFF;
-
-       TPM2_BASE_PTR->CONTROLS[0].CnSC = 0b1101 << 2;//TPM_CnSC_MSB_MASK | TPM_CnSC_ELSA_MASK;
-       TPM2_BASE_PTR->CONTROLS[0].CnV = 1;
-       TPM2_BASE_PTR->CONTROLS[0].CnSC |= 1<<6;
+		//CMOD set increment on each incoming clock pulse
+		//divide incoming clock by prescaler. 0b000 = 1, 0b111 = 128
+		TPM0_SC = TPM_SC_CMOD(1) |TPM_SC_PS(6);
+		
+		// set to 0xFFFE to leave non-triggering value at 0xFFFF
+		TPM0_MOD = TPM_MOD_VAL;
+		
+		//setup interrupt
+		int_init(INT_TPM0, priority_1);
+		
+		int n;
+		for(n = 2; n < 6; n++)
+		{
+			//configd for output compare
+			TPM0_CnSC(n) = TPM_CnSC_MSB_MASK | TPM_CnSC_MSA_MASK | TPM_CnSC_ELSA_MASK;
+		
+			//initialized to "safe" off value at 0xFFFF
+			//TPM0_CnV(0) = TPM_MOD_VAL_OFF;
+			//temporary
+			TPM0_CnV(n) = 1;
+		
+			//ACTIVATE INTERRUPT. 3. 2. 1. GOOOOOOOOOOOOOOOOOOOOO
+			TPM0_CnSC(n) |= TPM_CnSC_CHIE_MASK;
+		}
 }
 
- 
-
-void FTM2_IRQHandler()
+void uc_tpm_set_callback(tpm_chan_t p_tpm_chan, callback_t p_callback)
 {
+	tpm_callback[p_tpm_chan] = p_callback;
+}
 
-       TPM2_BASE_PTR->CONTROLS[0].CnV = (TPM2_BASE_PTR->CONTROLS[0].CnV+2);
-       TPM2_BASE_PTR->CONTROLS[0].CnSC |= 1 << 7;
+void TPM0_IRQHandler()
+{
+	int n = 2;
+	// loop through all used tpm channels
+	for(n = 2; n < 6; n++)
+		//check if interrupt occurred in that channel 
+		if((TPM0_CnSC(n) & TPM_CnSC_CHF_MASK) == TPM_CnSC_CHF_MASK)
+		{
+			TPM0_CnSC(n) |= TPM_CnSC_CHF_MASK;
+			TPM0_CnV(n) = ((TPM0_CnV(n)+n)%TPM_MOD_VAL);
+			//check if callback is set
+			if(tpm_callback[n])
+			{
+				tpm_callback[n]();
+			}
+		}
+		//reload
+		//TPM0_CnV(0) = (TPM0_CnV(0)+2);
+		
+
 
 }
