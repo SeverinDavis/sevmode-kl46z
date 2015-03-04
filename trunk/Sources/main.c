b04b71be409ea09e0ed3fb8437cf9f1eff34f183
severin.davis@gmail.com <severin.davis@gmail.com@d5b178a0-251e-a8bb-41f0-a278b1469bd0>
2015-03-04 03:58:24 +0000

    added failure tolerance on XBee

--- trunk/Sources/main.c
+++ trunk/Sources/main.c
@@ -15,15 +15,23 @@
 #include "CAR_XBEE.h"
 #include "uc_uart.h"
 
-#define PCKT_NUM 9
+#define PCKT_NUM 10
+
+typedef enum{
+	wait,
+	first,
+	second,
+	valid
+} xbee_state;
 
 int switch_3_push = 0;
 int switch_1_push = 0;
 int xbee_pckt_cnt = 0;
-char raw_pckts[PCKT_NUM] = {0,0,0,0,0,0,0,0,0}; 
+char raw_pckts[PCKT_NUM] = {0,0,0,0,0,0,0,0,0,0}; 
 int first_message = 0;
 int deadman = 0;
 int led_update = 0;
+xbee_state xb_s = wait;
 
 //has to run at lower rate than xbee callback
 void PIT0_CALLBACK()
@@ -108,53 +116,106 @@ void XBEE_CALLBACK()
 		CAR_LED_set_color(car_led_3, car_led_red);
 		led_update = 1;
 	}
+	char rcv = uc_uart_get_data();
 	
-	//sets deadman variable to check for active host
-	deadman = 1;
-	
-	//retrieves received data
-	raw_pckts[xbee_pckt_cnt] = uc_uart_get_data();
-	
-	//last packet reached
-	if(xbee_pckt_cnt == PCKT_NUM - 1)
+	//WAIT STATE
+	if(xb_s == wait)
 	{
-		//process these now so we keep TPM interrupt lockout as short as possible.
-			
-		unsigned int period0 = 0;
-		unsigned int period1 = 0;
-		unsigned int period2 = 0;
-		unsigned int period3 = 0;
-		
-		period0 = (raw_pckts[2] << 8) | raw_pckts[1];
-		period1 = (raw_pckts[4] << 8) | raw_pckts[3];
-		period2 = (raw_pckts[6] << 8) | raw_pckts[5];
-		period3 = (raw_pckts[8] << 8) | raw_pckts[7];
-		
-		int direction0 = raw_pckts[0] & 1;
-		int direction1 = (raw_pckts[0] >> 1) & 1;
-		int direction2 = (raw_pckts[0] >> 2) & 1;
-		int direction3 = (raw_pckts[0] >> 3) & 1;
-										
-		//access control, because TPM uses these. 
-		//we can't modify them while TPM is using them.
-		uc_tpm_mask_int();
-		 //update actuals
-		CAR_MOTOR_set_t_period(motor_0, period0);
-		CAR_MOTOR_set_t_period(motor_1, period1);
-		CAR_MOTOR_set_t_period(motor_2, period2);
-		CAR_MOTOR_set_t_period(motor_3, period3);
-		CAR_MOTOR_set_t_direction(motor_0, direction0);
-		CAR_MOTOR_set_t_direction(motor_1, direction1);
-		CAR_MOTOR_set_t_direction(motor_2, direction2);
-		CAR_MOTOR_set_t_direction(motor_3, direction3);
-		//set update flags in tpm
-		CAR_MOTOR_set_flags();
-
-		uc_tpm_unmask_int(); 
+		//if rcv is 0, move state to first
+		if(rcv == 0x00)
+		{
+			xb_s = first;
+		}
+		//otherwise stay in wait state
 	}
 	
-	//increment and loop around
-	xbee_pckt_cnt = (xbee_pckt_cnt + 1)%PCKT_NUM;
+	//FIRST STATE
+	else if(xb_s == first)
+	{
+		//if second 0 received move to second
+		if(rcv == 0x00)
+		{
+			xb_s = second;
+		}
+		//else something went wrong and we wait again
+		else
+		{
+			xb_s = wait;
+		}
+	}
+	
+	//SECOND STATE
+	else if(xb_s == second)
+	{
+		//loop back to second state again if 0
+		if(rcv == 0x00)
+		{
+			xb_s = second;
+		}
+		//we move into valid state
+		else
+		{
+			xb_s = valid;
+			//we set counter
+			xbee_pckt_cnt = 0;
+			//we move first valid character into raw rcv buffer
+			raw_pckts[xbee_pckt_cnt] = rcv;
+			xbee_pckt_cnt++;
+		}
+	}
+	
+	//VALID STATE
+	else if(xb_s == valid)
+	{
+		raw_pckts[xbee_pckt_cnt] = rcv;
+		xbee_pckt_cnt++;
+		
+		if(xbee_pckt_cnt == 10)
+		{
+			xb_s = wait;
+			
+			char check = raw_pckts[0] ^ raw_pckts[1] ^ raw_pckts[2] ^ raw_pckts[3] ^ raw_pckts[4] ^ raw_pckts[5] ^ raw_pckts[6] ^ raw_pckts[7] ^ raw_pckts[8];
+			
+			if(check == raw_pckts[9])
+			{
+				//process these now so we keep TPM interrupt lockout as short as possible.
+				unsigned int period0 = 0;
+				unsigned int period1 = 0;
+				unsigned int period2 = 0;
+				unsigned int period3 = 0;
+								
+				period0 = (raw_pckts[2] << 8) | raw_pckts[1];
+				period1 = (raw_pckts[4] << 8) | raw_pckts[3];
+				period2 = (raw_pckts[6] << 8) | raw_pckts[5];
+				period3 = (raw_pckts[8] << 8) | raw_pckts[7];
+								
+				int direction0 = raw_pckts[0] & 1;
+				int direction1 = (raw_pckts[0] >> 1) & 1;
+				int direction2 = (raw_pckts[0] >> 2) & 1;
+				int direction3 = (raw_pckts[0] >> 3) & 1;
+																
+				//access control, because TPM uses these. 
+				//we can't modify them while TPM is using them.
+				uc_tpm_mask_int();
+				 //update actuals
+				CAR_MOTOR_set_t_period(motor_0, period0);
+				CAR_MOTOR_set_t_period(motor_1, period1);
+				CAR_MOTOR_set_t_period(motor_2, period2);
+				CAR_MOTOR_set_t_period(motor_3, period3);
+				CAR_MOTOR_set_t_direction(motor_0, direction0);
+				CAR_MOTOR_set_t_direction(motor_1, direction1);
+				CAR_MOTOR_set_t_direction(motor_2, direction2);
+				CAR_MOTOR_set_t_direction(motor_3, direction3);
+				//set update flags in tpm
+				CAR_MOTOR_set_flags();
+				
+				uc_tpm_unmask_int(); 
+				//sets deadman variable to check for active host
+				deadman = 1;
+				
+			}
+		}
+	}
 }
 
 /*
@@ -195,7 +256,7 @@ void init()
 	uc_uart_set_callback(XBEE_CALLBACK);
 	
 	//must be lower frequency than UART receive frequency
-	pit_init(pit_0, priority_3, 250000, PIT0_CALLBACK);
+	pit_init(pit_0, priority_3, 1000000, PIT0_CALLBACK);
 	pit_enable(pit_0);
 	
 	//enable interrupts
